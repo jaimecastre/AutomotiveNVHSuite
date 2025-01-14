99a4a54ac8cef889809862d9569f1478395821fa
Jacob Henriksen <jacob.henriksen@hbkworld.com>
2025-01-14 13:48:57 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NvhSuiteCalculation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
